971af6bd23cf9fc7e6602871a07325bdaaa0db13
parkshinehye <qkrtlsgp0996@gmail.com>
2024-09-24 11:56:16 +0900

    캐릭터 화면 출력 구현

--- character_grass.py
+++ character_grass.py
@@ -7,15 +7,17 @@ character = load_image('character.png')
 
 
 def run_circle():
-      print("rec")
-      
+      clear_canvas_now()
+      character.draw_now(400, 300)
+      delay(0.1)
       
 def run_rec():
-      print("circle")
+      print("run_rec")
 
 
 while True:
       run_rec()
       run_circle()
+      break
 
 close_canvas()
